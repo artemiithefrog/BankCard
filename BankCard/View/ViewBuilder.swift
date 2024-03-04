70719a0bf74f8c95f6b72e5ac379d21ca2a487b6
Artemii <artemiithefrog95@icloud.com>
2024-03-04 21:40:51 +0100

    Initial Commit

---
+++ BankCard/View/ViewBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  ViewBuilder.swift
+//  BankCard
+//
+//  Created by artemiithefrog . on 04.03.2024.
+//
+
+import Foundation
---
+++ BankCard/View/ViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  ViewManager.swift
+//  BankCard
+//
+//  Created by artemiithefrog . on 04.03.2024.
+//
+
+import Foundation
